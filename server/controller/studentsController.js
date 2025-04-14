117f9e6ffb02c88f214a4b16756083cb2375f323
MaxSolodovnikov <maxmixir@gamil.com>
2025-04-14 20:23:46 +0400

    добавил возможность добавлять пользователей и получать

--- server/controller/studentsController.js
+++ server/controller/studentsController.js
@@ -23,6 +23,33 @@ class StudentsController {
             next(err);
         }
     }
+    async addUser(req, res, next) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                throw ApiError.BadRquest("Ошибка валидации", errors.array());
+            }
+            const { phone, full_name_parent, full_name_student, password } =
+                req.body;
+            const student = await Students.create({
+                phone,
+                full_name_parent,
+                full_name_student,
+                password,
+            });
+            res.status(200).json(student);
+        } catch (err) {
+            next(err);
+        }
+    }
+    async getAllUsers(req, res, next) {
+        try {
+            const students = await Students.findAll();
+            res.status(200).json(students);
+        } catch (err) {
+            next(err);
+        }
+    }
 }
 
 module.exports = new StudentsController();
